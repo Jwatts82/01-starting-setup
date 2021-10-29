8662a7845f211efc7675e372c42b9ca16c658a85
Jwatts82 <wattscodes@gmail.com>
2021-10-29 14:56:58 -0700

    create expense form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NewExpense.css'
+
+function NewExpense() {
+    return  
+        <div className="new-expense">
+            <form>
+
+            </form>
+        </div>
+}
+
+
+export default NewExpense
