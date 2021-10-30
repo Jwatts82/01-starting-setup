24f89db7a897c8488887a426e2d7a71cb31318d6
Jwatts82 <wattscodes@gmail.com>
2021-10-30 09:43:50 -0700

    add key

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-import Card from '../UI/Card'
-import ExpenseFilter from './ExpenseFilter'
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 // import { useState } from 'react/cjs/react.development';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020')
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = selectedYear => {
-    setFilteredYear(selectedYear)
-  }
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className='expenses'>
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map(expense => (
-      <ExpenseItem 
-        title={expense.title} 
-        amount={expense.amoutn} 
-        date={expense.date}
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amoutn}
+          date={expense.date}
         />
-     ))}
+      ))}
     </Card>
   );
 }
